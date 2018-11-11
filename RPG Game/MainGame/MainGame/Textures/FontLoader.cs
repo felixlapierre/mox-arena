1f1b38272a620a1c39ed3d2f0be4cfd66c376889
Felix <felix.lapierre1@gmail.com>
2018-11-11 15:23:46 -0500

    Added content loaders

---
+++ RPG Game/MainGame/MainGame/Textures/FontLoader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MainGame.Textures
+{
+    class FontLoader : ContentLoader<SpriteFont>
+    {
+        public FontLoader(ContentManager content) : base(content, "fonts")
+        {
+
+        }
+    }
+}
---
+++ RPG Game/MainGame/MainGame/Textures/TextureLoader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace MainGame.Textures
+{
+    class TextureLoader : ContentLoader<Texture2D>
+    {
+        public TextureLoader(ContentManager content, string root) : base(content, "graphics/" + root)
+        {
+            //Leave root blank to load textures in graphics folder
+        }
+    }
+}
