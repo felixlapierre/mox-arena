809e2ae118c39eb0d09d351e11c9b15384419f9e
arejayelle <35614138+arejayelle@users.noreply.github.com>
2018-11-11 18:33:28 -0500

    Added handlers for Keypress and MouseClick

---
+++ RPG Game/MainGame/MainGame/EventHandlers/KeyPressHandler.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainGame.ButtonManager
+{
+    public delegate void OnKeyPress();
+
+    class KeyPressHandler
+    {
+        private Keys key;
+        private OnKeyPress onKeyPress;
+        private bool keyPreviouslyPressed;
+
+
+        public KeyPressHandler(Keys key, OnKeyPress onKeyPress)
+        {
+            this.key = key;
+            this.onKeyPress = onKeyPress;
+            keyPreviouslyPressed = false;
+        }
+
+        public void Update()// this is the function that calls the delegate
+        {
+            KeyboardState keyboard = Keyboard.GetState();
+            if (keyboard.IsKeyDown(key)&& !keyPreviouslyPressed)
+            {
+                    onKeyPress.Invoke(); 
+            }
+
+            keyPreviouslyPressed = keyboard.IsKeyDown(key);
+        }
+    }
+
+}
---
+++ RPG Game/MainGame/MainGame/EventHandlers/MouseClickHandler.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainGame.ButtonManager
+{
+    public delegate void OnClick();
+    class MouseClickHandler
+    {
+        private ButtonState button;
+        private OnClick onClick;
+        private bool buttonPreviouslyPressed;
+
+        public MouseClickHandler(ButtonState button, OnClick onClick)
+        {
+            this.button = button;
+            this.onClick = onClick;
+            buttonPreviouslyPressed = false;
+        }
+        
+        // TODO find way to get rid of parameter in update method
+        public void Update(ButtonState button) 
+        {
+            MouseState mouse = Mouse.GetState();
+            if ((button== ButtonState.Pressed) && !buttonPreviouslyPressed)
+            {
+                    onClick.Invoke();
+            }
+            buttonPreviouslyPressed = true;
+        }
+    }
+
+}
